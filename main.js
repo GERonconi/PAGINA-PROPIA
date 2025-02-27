2a21cf2785487153a60fe376616e391e233b0600
Gustavo Ronconi <gerprofessional@hotmail.com>
2025-02-27 12:49:23 -0300

    quieroendo agregar un popover al textarea del formulario!

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function autoExpand(field) {
     field.style.height = 'auto';  // Reseteamos la altura para calcular el nuevo valor
     field.style.height = (field.scrollHeight) + 'px';  // Ajustamos la altura según el contenido
   }
+
+  $(document).ready(function () {
+    // Inicializar popover
+    $('[data-toggle="popover"]').popover();
+});
