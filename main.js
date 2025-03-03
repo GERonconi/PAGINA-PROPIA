406e9021398845bfd5954b91060567d8c8a4c01f
GERonconi <gerprofessional@hotmail.com>
2025-03-03 18:22:17 -0300

    se limpiaron un poco los archivos

--- main.js
+++ main.js
@@ -2,8 +2,4 @@ function autoExpand(field) {
     field.style.height = 'auto';  // Reseteamos la altura para calcular el nuevo valor
     field.style.height = (field.scrollHeight) + 'px';  // Ajustamos la altura según el contenido
   }
-
-  $(document).ready(function () {
-    // Inicializar popover
-    $('[data-toggle="popover"]').popover();
-});
+;
--- quienes-somos.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Cargar el encabezado desde header.html
-    fetch("quienessomos.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("header-placeholder").innerHTML = data;
-        })
-        .catch(error => console.error("Error al cargar el encabezado:", error));
-});
